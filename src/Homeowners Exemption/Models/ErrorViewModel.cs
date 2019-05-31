9c52decba371410df018142fb76c4299336da610
Daniel <dspencer@assessor.lacounty.gov>
2019-05-31 15:47:39 -0700

    initial commit

---
+++ src/Homeowners Exemption/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Homeowners_Exemption.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
